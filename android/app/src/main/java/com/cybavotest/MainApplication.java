f766a0990c3cb9ebb3772087230d6eefed807229
Anderson Chen <twan0817@gmail.com>
2019-09-18 17:56:41 +0800

    Add cybavo sdk

--- android/app/src/main/java/com/cybavotest/MainApplication.java
+++ android/app/src/main/java/com/cybavotest/MainApplication.java
@@ -3,6 +3,7 @@ package com.cybavotest;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.cybavo.reactnative.wallet.service.CybavoWalletServicePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new CybavoWalletServicePackage()
       );
     }
 
