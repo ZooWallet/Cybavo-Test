fce230ec0157a8bc1ce479f895a241af55a9d477
Anderson Chen <twan0817@gmail.com>
2019-09-19 12:47:36 +0800

    Add a swift file

---
+++ ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  CybavoTest
+//
+//  Created by Anderson Chen OG on 2019/9/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
---
+++ ios/Pods/Target Support Files/Pods-CybavoTest/Pods-CybavoTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CybavoTest : NSObject
+@end
+@implementation PodsDummy_Pods_CybavoTest
+@end
