43b40195193c141c17bd28fd64ecd782c613c90a
Anderson Chen <twan0817@gmail.com>
2019-09-19 12:25:02 +0800

    Update Podfile

---
+++ ios/Pods/CYBAVOWallet/CYBAVOWallet.framework/Headers/CYBAVOWallet.h
@@ -0,0 +1,19 @@
+//
+//  CYBAVOWallet.h
+//  CYBAVOWallet
+//
+//  Created by Vincent Huang on 2018/12/18.
+//  Copyright © 2018 Cybavo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CYBAVOWallet.
+FOUNDATION_EXPORT double CYBAVOWalletVersionNumber;
+
+//! Project version string for CYBAVOWallet.
+FOUNDATION_EXPORT const unsigned char CYBAVOWalletVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CYBAVOWallet/PublicHeader.h>
+
+
